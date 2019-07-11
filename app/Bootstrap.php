2aeed5b3ddb06577216b62459fb45d8a14912444
GoStars <gostars111@gmail.com>
2019-07-11 18:07:04 +0300

    Add error handler

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -1,2 +1,28 @@
 <?php
-    echo 'Hello World!';
+    namespace Myblog2;
+
+    use Whoops\Run;
+    use Whoops\Handler\PrettyPageHandler;
+
+    require __DIR__ . '/../vendor/autoload.php';
+
+    error_reporting(E_ALL);
+
+    $environment = 'development';
+
+    /**
+     * Ragister the error handler
+    */
+    $whoops = new Run;
+
+    if ($environment !== 'production') {
+        $whoops->pushHandler(new PrettyPageHandler);
+    } else {
+        $whoops->pushHandler(function($e) {
+            echo 'Todo: Friendly error page and send an email to the developer';
+        });
+    }
+
+    $whoops->register();
+
+    throw new \Exception;
